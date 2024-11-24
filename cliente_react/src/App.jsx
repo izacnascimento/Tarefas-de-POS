fe3a9b1920c26daa0406dd84e628ca2f407784b4
izacnascimento <115838994+izacnascimento@users.noreply.github.com>
2024-11-24 08:49:44 -0300

    ok

---
+++ cliente_react/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Index from './pages/Index';
+import Marcas from './pages/Marcas';
+import Modelos from './pages/Modelos';
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+    <Menu></Menu>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/marcas" element={<Marcas />} />
+            <Route path="/modelos" element={<Modelos />} />
+          </Routes>
+        </div>
+      </Router>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
